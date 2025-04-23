8b4f1095447736e237764985b6f825affcc98ea7
Jianfeng Liu <liujianfeng1994@gmail.com>
2025-04-23 10:24:41 +0800

    add vblade loongarch binary

--- VBLADE/vblade-master/build.sh
+++ VBLADE/vblade-master/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-rm -f vblade_*
+rm -f vblade_64 vblade_32 vblade_aa64
 
 gcc linux.c aoe.c ata.c bpf.c -Os -o vblade_64
 gcc linux.c aoe.c ata.c bpf.c -Os -m32 -o vblade_32
 aarch64-buildroot-linux-uclibc-gcc linux.c aoe.c ata.c bpf.c -Os -static -o vblade_aa64
 
-if [ -e vblade_64 ] && [ -e vblade_32 ] && [ -e vblade_aa64 ]; then
+if [ -e vblade_64 ] && [ -e vblade_32 ] && [ -e vblade_aa64 ] && [ -e vblade_la64 ]; then
     echo -e '\n################## SUCCESS ######################\n'
 else
     echo -e '\n################## FAILED ######################\n'
